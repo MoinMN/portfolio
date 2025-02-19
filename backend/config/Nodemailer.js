144ab0b9af812a3099d40c316abb506117a3112f
MoinMN <moinnaik45@eng.rizvi.edu.in>
2025-02-20 00:19:28 +0530

    email updated to titan's

--- backend/config/Nodemailer.js
+++ backend/config/Nodemailer.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.titan.email",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_ADDRESS,
     pass: process.env.EMAIL_PASSCODE,
