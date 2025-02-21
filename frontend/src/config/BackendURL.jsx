7d862d3acd355740cd239f26bdfba13c4a5094de
MoinMN <moinnaik45@eng.rizvi.edu.in>
2025-02-22 01:18:54 +0530

    add download app icon

--- frontend/src/config/BackendURL.jsx
+++ frontend/src/config/BackendURL.jsx
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://moinmn-api.vercel.app';
-const BASE_URL = 'http://localhost:4518';
+const BASE_URL = 'https://moinmn-api.vercel.app';
+// const BASE_URL = 'http://localhost:4518';
 
 
 export default BASE_URL;
