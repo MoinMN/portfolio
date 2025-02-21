616b6315dfb67109c90fedd99ed5488cff29a0d9
MoinMN <moinnaik45@eng.rizvi.edu.in>
2025-02-22 01:17:14 +0530

    add download app icon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { registerSW } from 'virtual:pwa-register';
 
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New update available. Reload to update?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 import sitemap from 'vite-plugin-sitemap';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,31 @@ export default defineConfig({
         { url: '/admin', changefreq: 'monthly', priority: 0.5 },
         { url: '/admin/dashboard', changefreq: 'weekly', priority: 0.6 },
       ],
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Moin MN | Portfolio",
+        short_name: "Moin MN",
+        description: "Discover my expertise in MERN Stack, Next.js, and full-stack web development. I specialize in building scalable, high-performance applications, delivering innovative solutions, and crafting seamless user experiences. Explore my projects, services, and technical skills.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/logo-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
     })
   ],
   resolve: {
