34e835cdceae445a6e9c1bdbd450fa1e62c68df9
MoinMN <moinnaik45@eng.rizvi.edu.in>
2025-02-22 01:24:47 +0530

    add download app icon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import { registerSW } from 'virtual:pwa-register';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
 
+// PWA Auto Update
 const updateSW = registerSW({
   onNeedRefresh() {
-    if (confirm("New update available. Reload to update?")) {
+    if (confirm("New version available. Reload?")) {
       updateSW(true);
     }
   },
-  onOfflineReady() {
-    console.log("App is ready to work offline.");
-  }
 });
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
